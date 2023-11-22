368718b84377385938d744bc463cf270688c48c9
emmystringzhs <milesbetti@gmail.com>
2023-11-22 18:37:09 +0100

    update the settings file allowed host

--- tm/settings.py
+++ tm/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-o60ncj!w8*@a#ksk)rc&v&0^-+t(br)*8a*h436lj1(06+j@x=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
